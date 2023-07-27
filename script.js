0fe4035010a5f4b462e7c3d9113530a203d83a83
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-27 23:01:24 +0200

    doplnil jsem komentáře

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+/********************************************************************
+    Načtení data z localStorage do proměnné names;
+    pokud je localStorage prázdný, tak do names se uloží prázdné pole
+*********************************************************************/
 const names = getSavedNames()
 
-
+/********************************************************************
+    Odeslání formuláře a uložení do localStorage pomocí proměnné names
+*********************************************************************/
 let myForm = document.querySelector("#test-form")
 myForm.addEventListener("submit", function(event){
     event.preventDefault()

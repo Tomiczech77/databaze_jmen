715c4d871a8aad1a7531e4787f24814ab5655fb9
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-27 23:27:52 +0200

    přidal jsem novou funci pro vypsání jména na stránku + vytvoření tlačítka pro smazaní jména

--- functions.js
+++ functions.js
@@ -20,3 +20,23 @@ const getSavedNames = function(){
 const saveNames = function(oneName){
     localStorage.setItem("names", JSON.stringify(oneName))
 }
+
+
+/***********************************************************************
+    Generování HTML struktury, kterou umístíme do stránky po kliknutí na tlačítko "Vypiš"
+    + použijeme ji také pro vypsíná nových informací z localStorage, když nějaké jméno vymažeme pomocí tlačítka "Vymazat jméno"
+************************************************************************/
+const generateHTMLstructure = function(oneName){
+    const newDiv = document.createElement("div")
+    const newSpan = document.createElement("span")
+    const button = document.createElement("button")
+
+    // Nastavení mazacího tlačítka
+    button.textContent = "Vymazat jméno"
+    newDiv.appendChild(button)
+
+    newSpan.textContent = oneName.firstName
+    newDiv.appendChild(newSpan)
+
+    return newDiv
+}

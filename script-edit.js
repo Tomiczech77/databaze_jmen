d8cf694c40b865f1806e285df6060c4d2f8c13d8
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-28 18:23:53 +0200

    vytažení id z url adtesy a hledá objekt v localStorage (dá se vypsat do console

---
+++ script-edit.js
@@ -0,0 +1,16 @@
+let nameID = location.hash.substring(1)
+let names = getSavedNames()
+
+
+let searchedName = names.find(function(oneObject){
+    return oneObject.id === nameID
+})
+
+
+if(searchedName === undefined){
+    location.assign("index.html")
+}
+
+
+//  jen kontrola - vymazat
+console.log(searchedName)

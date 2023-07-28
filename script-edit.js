a6228590bcf348f6e7e8564d56776f3d1ebb9925
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-28 18:59:18 +0200

    změněné jméno se ukládá do localStorage

--- script-edit.js
+++ script-edit.js
@@ -1,16 +1,24 @@
 let nameID = location.hash.substring(1)
 let names = getSavedNames()
 
-
+// Vytažení objektu
 let searchedName = names.find(function(oneObject){
     return oneObject.id === nameID
 })
 
-
+// Když id nelze najít, přesměruje nás to automaticky na stránku index.html
 if(searchedName === undefined){
     location.assign("index.html")
 }
 
+// Vypíše se nám jméno do inputu
+document.querySelector("#edited-name").value = searchedName.firstName
 
-//  jen kontrola - vymazat
-console.log(searchedName)
+// Můžeme změnit jméno v input a změnit ho u celého objektu
+let changingForm = document.querySelector("#changing-form").addEventListener("submit", function(event){
+    event.preventDefault()
+
+    searchedName.firstName = event.target.elements.changingName.value
+
+    saveNames(names)
+})

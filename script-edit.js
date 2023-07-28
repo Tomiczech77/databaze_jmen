7b3b1f87612f1f89925905620bba6ec139c029f1
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-28 19:19:26 +0200

    aktualizuje data napříč záložkami v prohlížeči

--- script-edit.js
+++ script-edit.js
@@ -22,3 +22,27 @@ let changingForm = document.querySelector("#changing-form").addEventListener("su
 
     saveNames(names)
 })
+
+/******************************************************************************************
+    Pokud se něco změní na jedné záložce, tak se to projeví i na dalších záložkach stejmé stránky
+******************************************************************************************/
+window.addEventListener("storage", function(event){
+    console.log(event)
+
+    if(event.key === "names"){
+        names = JSON.parse(event.newValue)
+    }
+
+    // Vypíše jméno toho, který má shodné id jako je id v url stránky
+    let searchedName = names.find(function(oneObject){
+        return oneObject.id === nameID
+    })
+    
+    // Když id nelze najít, přesměruje nás to automaticky na stránku index.html
+    if(searchedName === undefined){
+        location.assign("index.html")
+    }
+    
+    // Vypíše se nám jméno do inputu
+    document.querySelector("#edited-name").value = searchedName.firstName
+})
